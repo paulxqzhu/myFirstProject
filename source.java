783d01aa396dd17e93b7541e684527a5ac6fb212
zhup <zhup@agi.ca>
2016-05-05 16:16:50 -0400

    commit for aCopy repo
    Conflicts:
    	source.java

--- source.java
+++ source.java
@@ -5,3 +5,15 @@ other lines
 
 more lines
 
+<<<<<<< HEAD
+=======
+
+this is to append something at the end of file
+
+end22222222222
+
+
+3333333
+3333333
+33333
+>>>>>>> 5bfb967... commit for aCopy repo
